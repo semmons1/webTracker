b5cedce51c6268660e1b752f462fa1a9230d5441
Stefan Emmons <stefan@debian>
2020-01-26 13:47:00 -0700

    Secure sign-in with Okta and basic tracking database

---
+++ src/webTracker/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using Okta.AspNetCore;
+
+namespace webTracker.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            if(!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge(OpenIdConnectDefaults.AuthenticationScheme);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Logout()
+        {
+            return new SignOutResult(new[]
+            {
+                OpenIdConnectDefaults.AuthenticationScheme,
+                CookieAuthenticationDefaults.AuthenticationScheme
+            });
+        }
+    }
+}
